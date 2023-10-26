8a11eae65dcd83a9a7e8b54c33bfd63686a1cfa4
Rone <atounkara203@gmail.com>
2023-10-26 18:06:35 +0000

    Envoie des fichiers

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 // Importation des modules nécessaires
 const express = require('express'); // Express.js pour créer le serveur
 const path = require('path'); // Path pour gérer les chemins de fichiers
+const engines = require('consolidate');
+
 
 // Crée une application Express
 const app = express();
 
 // Définition du dossier statique "public" pour servir des fichiers statiques (CSS, JS, images, etc.)
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + '/public');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
 
 // Définition du numéro de port pour le serveur
 const port = 3500;
 
 // Gère la route principale ("/") pour renvoyer le fichier "accueil.html"
+//app.get('/', (req, res) => {
+//    res.sendFile(path.join(__dirname + '/public/accueil.html'));
+//})
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/accueil.html'));
+    res.render('accueil');
+})
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
+app.get('/profil', (req, res) => {
+    res.render('profil');
 })
 
 // gestion des erreurs 404
 app.use((req, res) => {
     // renvoie le fichier "404.html quand on a le  statut 404 (Page non trouvée)
-    res.status(404).sendFile(path.join(__dirname + '/public/404.html'));
+    res.status(404).render('404');
 })
 
 // Démarre le serveur Express et écoute le port spécifié en haut
